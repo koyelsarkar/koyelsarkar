174492863dc2190d7e19b16144d323ca761520fd
koyelsarkar <koyel.srkr27@gmail.com>
2013-01-31 08:59:03 +0530

    deleted

--- myapplication/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Myapplication::Application.config.secret_token = '5889b0623368e31ec702c4da505e99d94ff607f52a9a13e2c80f28caa2226809665d99819fb4f8f519c8276ce481e0185228e433e7c1ec1a716e9dc2b9e96ae2'
